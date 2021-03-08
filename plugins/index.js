8d301d9d862eb52f0b68bc45a87037c1810b4ccd
Martin Nicholson <martin@taotesting.com>
2021-03-08 16:14:32 +0100

    feat: extra config loader in plugins init

--- plugins/index.js
+++ plugins/index.js
@@ -16,34 +16,51 @@
  * Copyright (c) 2019-21 (original work) Open Assessment Technologies SA ;
  */
 
-const path = require('path');
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
 const { initPlugin: initSnapshotPlugin } = require('cypress-plugin-snapshots/plugin');
 
+const fs = require('fs-extra');
+const path = require('path');
+
 /**
- * Applies values from config.env to the main config
- * Also responsible for init of third-party plugins
+ * Reads env config from file
+ * @see https://docs.cypress.io/api/plugins/configuration-api.html#Switch-between-multiple-configuration-files
+ * @param {String} envConfigFile
+ * @returns {Promise} resolves to JSON
+ */
+function getConfigurationByFile(envConfigFile) {
+    const pathToEnvConfigFile = path.resolve(envConfigFile);
+    return fs.readJson(pathToEnvConfigFile);
+}
+
+/**
+ * Responsible for init of third-party plugins
+ * Also extends config object
+ * This function is called when a project is opened or re-opened (e.g. due to the project's config changing)
  *
- * @param {function} on // `on` is used to hook into various events Cypress emits
- * @param {Object} config // `config` is the resolved Cypress config
+ * @type {Cypress.PluginConfig}
+ * @param {function} on used to hook into various events Cypress emits
+ * @param {Object} config the resolved Cypress config
  */
 module.exports = (on, config) => {
-    const { integrationFolder, testFiles, baseUrl, testName } = config.env;
-
-    if (integrationFolder) {
-        config.integrationFolder = integrationFolder;
-    }
-    if (testFiles) {
-        config.testFiles = testFiles;
-    }
-    if (baseUrl) {
-        config.baseUrl = baseUrl;
-    }
-    if (testName) {
-        config.testFiles = path.join('**', testName, config.testFiles);
-    }
-
+    // plugin inits
     initSnapshotPlugin(on, config);
 
+    // extend main config with env config, if env.configFile key exists
+    if (config && config.env && config.env.configFile) {
+        return getConfigurationByFile(config.env.configFile).then(configJson => {
+            Object.assign(config.env, configJson);
+            return config;
+        });
+    }
     return config;
 };
-
