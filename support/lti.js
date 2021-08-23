c5e7e6aa4df982ae2863566ca2b5599488b593dd
Andres Rosales <andresrosalespinzon@gmail.com>
2021-08-23 10:58:30 +0200

    chore: updated getLtiLaunchUrl JSDoc example

--- support/lti.js
+++ support/lti.js
@@ -289,11 +289,19 @@ Cypress.Commands.add('ltiLaunchViaTool', options => {
  * (https://github.com/oat-sa/demo-lti1p3/blob/master/doc/api.md#ltiresourcelinkrequest-launch-generation-endpoint)
  *
  * @example
- * cy.ltiLaunchViaTool({
+ * cy.getLtiLaunchUrl({
  *   toolUrl: 'http://demo.lti.app/api/launch',
  *   authToken: 'q4s7fv80n9eq5z5f7fgs',
  *   registration: 'default',
- *   ltiResourceId: '0d3d8b41-7af1-4ad1-9fc0-5f9b1db23287'
+ *   ltiResourceId: '0d3d8b41-7af1-4ad1-9fc0-5f9b1db23287',
+ * body: {
+            target_link_uri: "https://testrunner-oat-demo.dev.gcp-eu.taocloud.org/api/v1/auth/launch-lti-1p3/44b26693d772",
+            registration,
+            claims,
+            user: {
+                id: "pepe"
+            }
+        }
  * });
  *
  * @param {ltiOptions} options
